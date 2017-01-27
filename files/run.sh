89e3a033387153284b82611cd6eeaa90da092740
Johannes 'fish' Ziemke <github@freigeist.org>
2017-01-27 19:34:48 +0100

    Check if wlan ip is already set

--- files/run.sh
+++ files/run.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 . /etc/environment
 
-ip addr add $WLAN_ADDRESS/$WLAN_NETMASK dev wlan0
+ip addr show wlan0 | grep "$WLAN_ADDRESS" \
+  || ip addr add $WLAN_ADDRESS/$WLAN_NETMASK dev wlan0
 
 export DHCP_RANGE=$DHCP_RANGE
 exec s6-svscan /etc/service
