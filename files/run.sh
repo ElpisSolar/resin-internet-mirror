24df542da11500f6619f5e99728a258f73d332b7
Johannes 'fish' Ziemke <github@freigeist.org>
2017-01-27 19:49:49 +0100

    Get rid of environment file again
    Somehow I can't get execlineb to properly interpolate the variable..

--- files/run.sh
+++ files/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-. /etc/environment
+
+# FIXME: Move dhcp range here
+WLAN_ADDRESS=10.10.0.1
+WLAN_NETMASK=22
 
 ip addr show wlan0 | grep "$WLAN_ADDRESS" \
   || ip addr add $WLAN_ADDRESS/$WLAN_NETMASK dev wlan0
 
-export DHCP_RANGE=$DHCP_RANGE
 exec s6-svscan /etc/service
