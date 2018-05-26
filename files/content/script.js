4f8f9f8c50d20b884aa74ca2610a51a11e352b8f
Samuel <s1436963@sms.ed.ac.uk>
2018-05-26 12:15:37 +0100

    Improve ux (#6)
    * update index
    
    * add full-screen overlay

---
+++ files/content/script.js
@@ -0,0 +1,13 @@
+//Javascript file
+
+//Click event open overlay button
+
+function openNav(){
+	document.getElementById("nav").style.height = "100%";
+	}
+
+//Click event close overlay button
+
+function closeNav(){
+	document.getElementById("nav").style.height = "0%";
+	}
